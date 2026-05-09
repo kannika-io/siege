0f42ec724fc312c2b12671757e8133b55344a6ec
Tom Verelst <664136+tomverelst@users.noreply.github.com>
2026-05-09 17:38:31 +0200

    feat(siege): add EventEmitter trait

--- crates/siege/src/event.rs
+++ crates/siege/src/event.rs
@@ -3,3 +3,7 @@ use std::any::Any;
 pub trait DomainEvent: Any + Send + Sync {
     fn event_name(&self) -> &'static str;
 }
+
+pub trait EventEmitter: Send + Sync + 'static {
+    fn emit(&self, event: &dyn DomainEvent);
+}
